11756c97d38afdcd23d844bed89768d64409bc1b
sara woodford <sarawoodford6@gmail.com>
2025-01-20 22:26:25 -0330

    feat: methods in AvgTemp
    - calculate average temperature
    - count the number of days above average

--- QAP1-Temp/AvgTemp.java
+++ QAP1-Temp/AvgTemp.java
@@ -1,4 +1,20 @@
 
 public class AvgTemp{
+    public static double CalcAverageTemp(double[] temperatures){
+        double sum = 0;
+        for (double temp : temperatures){
+            sum += temp;
+        }
+        return sum / temperatures.length;
+    }
 
+    public static int daysAboveAverage(double[] temperatures, double averageTemp){
+        int count = 0;
+        for(double temp : temperatures){
+            if (temp > averageTemp){
+                count++;
+            }
+        }
+        return count;
+    }
 }
