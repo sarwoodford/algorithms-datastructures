dee7dfd8a5cd9d07617267e9f087734412c400b2
sara woodford <sarawoodford6@gmail.com>
2025-01-23 00:13:20 -0330

    feat: menu display and function skeletons
    - menu prompts user to enter an option to
    complete specific tasks
    - validation to reprompt user if invalid menu
    option is selected
    - skeletons for menu option functions created

--- QAP2/MovieTheatre.java
+++ QAP2/MovieTheatre.java
@@ -1,4 +1,51 @@
 package QAP2;
+
+import java.util.Scanner;
 public class MovieTheatre {
-    
+    public static void displayMenu(){
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            while(true){
+                System.out.println("\nWelcome to the Movie Theatre! Please Browse our menu options: ");
+                System.out.println("1. Reserve Seat(s)");
+                System.out.println("2. Cancel Seat(s)");
+                System.out.println("3. View Seating Chart");
+                System.out.println("4. Exit");
+                System.out.println("");
+                System.out.println("Please enter your option: ");
+
+                int choice = scanner.nextInt();
+
+                switch(choice){
+                    case 1: 
+                        reserveSeats(scanner);
+                        break;
+                    case 2: 
+                        cancelSeats(scanner);
+                        break;
+                    case 3:
+                        viewSeatingChart();
+                        break;
+                    case 4:
+                        System.out.println("Thanks for visiting! Exiting menu...");
+                        return;
+                    default: 
+                        System.out.println("INvalid menu option entered. Please enter 1, 2, 3 or 4.");
+                }
+            }
+        }
+    }
+
+    public static void reserveSeats(Scanner scanner){
+
+    }
+
+    public static void cancelSeats(Scanner scanner){
+
+    }
+
+    public static void viewSeatingChart(){
+
+    }
 }
+
