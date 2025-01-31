143acd4a4b1f73b88901d8c147ee0f230bc215c2
sara woodford <sarawoodford6@gmail.com>
2025-01-31 16:05:00 -0330

    feat: implementing provided boiler plate code to work around

--- QAP3/UndoRedo.java
+++ QAP3/UndoRedo.java
@@ -1,5 +1,34 @@
 package QAP3;
 
-public class UndoRedo {
+public class UndoRedo<T> {
+    private class Node {
+        private T state;
+        private Node prev;
+        private Node next;
+        private Node (T state) {
+            this.state = state;
+        }
     
+    }
+    private Node currentState;
+    //Undo operation
+    public T undo(){
+    //implement me
+    }
+    
+    //perform an operation
+    public void  addState (T newState) {
+        //implement me
+    
+    }
+    
+    //Redo Operation
+    public T redo(){
+        //implement me
+    }
+    
+    public static void main(String[] args) {
+    
+    
+    }
 }
