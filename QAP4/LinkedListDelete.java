4e4352da5b2fff0d3f32bb0fcfce7c29c4579a0b
sara woodford <sarawoodford6@gmail.com>
2025-02-03 11:45:13 -0330

    feat: genersl structure of LinkedListDelete
    - constucter done
    - head initialized
    - methods created but not implemented

--- QAP4/LinkedListDelete.java
+++ QAP4/LinkedListDelete.java
@@ -1,5 +1,39 @@
 package QAP4;
 
-public class LinkedListDelete {
+public class LinkedListDelete<T>{
+    class Node{
+        int listItem;
+        Node next;
     
+        public Node(int listItem){
+            this.listItem = listItem;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+
+    public LinkedListDelete(){
+        this.head = null;
+    }
+
+    public T errorIfDoesntExist(){
+
+    }
+
+    public T deleteAtBeginning(){
+
+    }
+
+    public T deleteAtEnd(){
+
+    }
+
+    public T deleteAnywhere(){
+
+    }
+
+    public static void main(String[] args){
+        
+    }
 }
