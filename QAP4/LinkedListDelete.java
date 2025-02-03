48de8a56bf3cbe7c6eab03f779e7c6acd0b756fb
sara woodford <sarawoodford6@gmail.com>
2025-02-03 12:03:04 -0330

    feat: methods implemenetd
    - display error if linked list doesn't exist
    - delete a list item from the beginning
    - delete a list item from the end
    - delete a list item anywhere

--- QAP4/LinkedListDelete.java
+++ QAP4/LinkedListDelete.java
@@ -2,10 +2,10 @@ package QAP4;
 
 public class LinkedListDelete<T>{
     class Node{
-        int listItem;
+        T listItem;
         Node next;
     
-        public Node(int listItem){
+        public Node(T listItem){
             this.listItem = listItem;
             this.next = null;
         }
@@ -18,22 +18,70 @@ public class LinkedListDelete<T>{
     }
 
     public T errorIfDoesntExist(){
+        if (head == null){
+            System.out.println("List is empty.");
+            return null;
+        }
 
+        return null;
     }
 
     public T deleteAtBeginning(){
-
+        if (head == null){
+            return errorIfDoesntExist();
+        }
+        T data = head.listItem;
+        head = head.next;
+        return data;
     }
 
     public T deleteAtEnd(){
+        if (head == null){
+            return errorIfDoesntExist();
+        }
 
+        if (head.next == null){
+            T data = head.listItem;
+            head = null;
+            return data;
+        }
+
+        Node current = head;
+
+        while(current.next != null && current.next.next != null){
+            current = current.next;
+        }
+
+        T data = current.next.listItem;
+        current.next = null;
+        return data;
     }
 
-    public T deleteAnywhere(){
+    public T deleteAnywhere(T item){
+        if ( head == null){
+            return errorIfDoesntExist();
+        }
 
+        if(head.listItem.equals(item)){
+            return deleteAtBeginning();
+        }
+
+        Node current = head;
+        while(current.next != null && !current.next.listItem.equals(item)){
+            current = current.next;
+        }
+
+        if (current.next == null){
+            System.out.println("item not found");
+            return null;
+        }
+
+        T data = current.next.listItem;
+        current.next = current.next.next;
+        return data;
     }
 
     public static void main(String[] args){
-        
+
     }
 }
