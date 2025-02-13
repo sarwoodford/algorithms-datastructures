be302fc6448f8387e464dfeb6289238c4ebbfff4
sara woodford <sarawoodford6@gmail.com>
2025-02-12 22:33:30 -0330

    feat: dequeueAny method
    - Animal class tracks all animals time within thier
    individual queues
    - dequeueAny allows user to adopt the "oldest"
    animal (dog/cat not specified)
    - updated Queue types for cat and dog individual
    methods to fix errors

--- QAP5/AnimalShelter.java
+++ QAP5/AnimalShelter.java
@@ -1,11 +1,39 @@
 package QAP5;
 
-import java.util.Queue;
 import java.util.LinkedList;
+import java.util.Queue;
+
+class Animal implements Comparable<Animal> {
+    private String animalInfo;
+    private long timestamp;
+
+    public Animal(String animalInfo) {
+        this.animalInfo = animalInfo;
+        this.timestamp = System.currentTimeMillis();
+    }
+
+    public String getAnimalInfo() {
+        return animalInfo;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    @Override
+    public int compareTo(Animal other) {
+        return Long.compare(this.timestamp, other.timestamp);
+    }
+
+    @Override
+    public String toString() {
+        return animalInfo + " (timestamp: " + timestamp + ")";
+    }
+}
 
 public class AnimalShelter {
-    private Queue<String> catQueue;
-    private Queue<String> dogQueue;
+    private Queue<Animal> catQueue;
+    private Queue<Animal> dogQueue;
 
     public AnimalShelter() {
         catQueue = new LinkedList<>();
@@ -13,18 +41,42 @@ public class AnimalShelter {
     }
 
     public void enqueueCat(String catInfo) {
-        catQueue.add(catInfo);
+        catQueue.add(new Animal(catInfo));
     }
 
     public void enqueueDog(String dogInfo) {
-        dogQueue.add(dogInfo);
+        dogQueue.add(new Animal(dogInfo));
     }
 
-    public String dequeueCat() {
+    public Animal dequeueCat() {
         return catQueue.poll();
     }
 
-    public String dequeueDog() {
+    public Animal dequeueDog() {
         return dogQueue.poll();
+    }
+
+    public Animal dequeueAny() {
+        Animal oldestCat = catQueue.peek();
+        Animal oldestDog = dogQueue.peek();
+
+        if (oldestCat == null && oldestDog == null) {
+            System.out.println("No animals available!");
+            return null;
+        } else if (oldestDog == null) {
+            return catQueue.poll();
+        } else if (oldestCat == null) {
+            return dogQueue.poll();
+        } else {
+            if (oldestCat.compareTo(oldestDog) <= 0) {
+                return catQueue.poll();
+            } else {
+                return dogQueue.poll();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
     }
 }
